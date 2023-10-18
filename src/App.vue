099c27dacc550ce46d87461ef85db4a42432256c
maianabertola <maia@we-are-ensemble.com>
2023-10-18 17:26:20 +0200

    fonts added

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@
 </template>
 
 <script lang="ts">
-import { RouterLink, RouterView } from "vue-router";
+import { RouterView } from "vue-router";
 import NavBar from "./components/NavBar.vue";
 export default {
   name: "",

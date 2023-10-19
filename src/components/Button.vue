c4d70e9b3ed4bea07b73fc889e4f580c72385d26
maianabertola <maia@we-are-ensemble.com>
2023-10-19 13:46:16 +0200

    sections promess, services done

---
+++ src/components/Button.vue
@@ -0,0 +1,29 @@
+<template>
+  <button
+    class="py-4 px-8 bg-black rounded-md aeonik text-gray-100 tracking-wider"
+  >
+    {{ cta }}
+  </button>
+</template>
+
+<script lang="ts">
+export default {
+  name: "Button",
+  data() {
+    return {};
+  },
+  methods: {},
+  props: {
+    cta: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped>
+.aeonik {
+  font-family: "Aeonik";
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,91 @@
-<script setup lang="ts">
-import TheWelcome from "../components/TheWelcome.vue";
+<template>
+  <div class="">
+    <section id="heroSection" class="min-h-screen paddingSection">
+      <div class="min-h-[85vh] flex justify-between items-end">
+        <h1>Cool <br />Tagline <br />about this</h1>
+        <div>
+          <img src="../assets/icons/Arrow.png" alt="steady owl arrow" />
+        </div>
+      </div>
+    </section>
+
+    <section id="promess" class="paddingSection">
+      <div class="flexCenter">
+        <h2>Cool title about CFO as a Service.</h2>
+        <p class="pb-[20vh]">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque
+          suscipit in velit euismod ultrices. Lorem ipsum dolor sit amet,
+          consectetur adipiscing elit. Pellentesque suscipit in velit euismod
+          ultrices.
+        </p>
+      </div>
+      <hr />
+    </section>
+    <section id="services" class="paddingSection">
+      <div class="flex flex-col justify-center items-center">
+        <div class="text-center">
+          <h2>Here’s a cool promess about my services.</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Pellentesque suscipit in velit euismod ultrices.
+          </p>
+        </div>
+        <ServiceCard
+          v-for="(service, index) in services"
+          :key="index"
+          :title="service.title"
+          :text="service.text"
+          :imageService="service.imageService"
+        ></ServiceCard>
+        <div>
+          <Button :cta="callToAction" class="mb-[20vh]"></Button>
+        </div>
+        <hr />
+      </div>
+    </section>
+    <section id="process" class="paddingSection">
+      <div class="flexCenter">
+        <h2>How we get things done?</h2>
+        <p>Well, it’s simple simple.</p>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script lang="ts">
+import ServiceCard from "@/components/ServiceCard.vue";
+import Button from "@/components/Button.vue";
+
+export default {
+  name: "HomeView",
+  data() {
+    return {
+      services: [
+        {
+          title: "FP&A",
+          text: "Text 1",
+          imageService: "../../public/assets/images/dummies.png",
+        },
+        {
+          title: "Fund Raising",
+          text: "Text 2",
+          imageService: "../../public/assets/images/dummies.png",
+        },
+        {
+          title: "Post Merger Integration (PMI)",
+          text: "Text 3",
+          imageService: "../../public/assets/images/dummies.png",
+        },
+      ],
+      callToAction: "Click for more",
+    };
+  },
+  methods: {},
+  components: {
+    ServiceCard,
+    Button,
+  },
+};
 </script>
 
-<template>
-  <main></main>
-</template>
+<style scoped></style>
